e668e37a5335f39724fb588201423687b0d17faf
rsamama <ryan.samama@ensae.fr>
2023-04-06 21:27:01 +0000

    test_s1q5_min_distance fait et representation de network.1.in

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -4,36 +4,36 @@ import graphviz
 from random import randint
 
 data_path = "input/"
-file_name = "network.10.in"
+file_name = "network.1.in"
 
-g = gr.graph_from_file("input/network.3.in")
-# print(g)
-# a = g.connected_components_set()
-# print(a)
+g = gr.graph_from_file(data_path + file_name)
+print(g)
+a = g.connected_components_set()
+print(a)
 # b = g.get_path_with_power(1, 20, 600)
 # print(b)
-# c = g.get_path_with_power(18340, 11738, 7000000)
-# print(c)
+c = g.get_path_with_power(1, 20, 50)
+print(c)
 # k = g.kruskal()
 # print(k)
 # d = k.connected_components_set()
 # print(d)
-# k.representation_graph("input/network.1.in", 1, 20)
+g.representation_graph("input/network.1.in", 1, 20)
 # print(g.min_power(1, 20))
 # g.representation_graph("input/network.1.in", 1, 4)
 
 # QUESTION 10  #
-S1 = []
-for i in range(1, 11):
-    S2 = 0
-    L1, L2 = gr.little_routes_extract(str(i))
-    g = gr.graph_from_file("input/network." + str(i) + ".in")
-    for j in range(1, 11):
-        t0 = time.perf_counter()
-        g.min_power(randint(1, int(L1[0])), randint(1, int(L1[0])))
-        t1 = time.perf_counter()
-        S2 += t1-t0
-    S1.append(int(L2[0])*S2/10)
+# S1 = []
+# for i in range(1, 11):
+#     S2 = 0
+#     L1, L2 = gr.little_routes_extract(str(i))
+#     g = gr.graph_from_file("input/network." + str(i) + ".in")
+#     for j in range(1, 11):
+#         t0 = time.perf_counter()
+#         g.min_power(randint(1, int(L1[0])), randint(1, int(L1[0])))
+#         t1 = time.perf_counter()
+#         S2 += t1-t0
+#     S1.append(int(L2[0])*S2/10)
 
-for i in range(len(S1)):
-    print('le temps d éxecution de min_power pour le fichier routes.', i, '.in est : ', S1[i])
+# for i in range(len(S1)):
+#     print('le temps d éxecution de min_power pour le fichier routes.', i, '.in est : ', S1[i])
--- tests/test_s1q5_min_distance.py
+++ tests/test_s1q5_min_distance.py
@@ -1,1 +1,20 @@
-# À compléter
+# This will work if ran from the root folder.
+import sys 
+sys.path.append("delivery_network")
+
+from graph import graph_from_file
+import unittest   # The test framework
+
+class Test_MinimalPower(unittest.TestCase):
+    def test_network04(self):
+        g = graph_from_file("input/network.04.in")
+        self.assertEqual(g.get_path_with_power(1, 4, 11), [1, 4])
+        self.assertEqual(g.get_path_with_power(1, 4, 10), [1, 2, 3, 4])
+
+    def test_network1(self):
+        g = graph_from_file("input/network.1.in")
+        self.assertEqual(g.get_path_with_power(1, 20, 50), [1, 20])
+        self.assertEqual(g.get_path_with_power(1, 20, 30), [1, 8, 20])
+
+if __name__ == '__main__':
+    unittest.main()
